e0b427bdd3692eb6d76b8e7dd1a810ebfb6727b5
Jason <jason.nordheim@gmail.com>
2023-09-11 22:04:38 -0600

    implemented basic graphql setup

--- example/src/index.js
+++ example/src/index.js
@@ -1,10 +1,29 @@
 import express from "express";
+import { graphqlHTTP } from "express-graphql"; // ES6
+import { buildSchema } from "graphql";
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
 
 const app = express();
-
-app.get("/", (_, res) => {
-  res.send("graphql running");
-});
-
-const PORT = 4040;
-app.listen(PORT, () => console.log(`Graphql is running on http://localhost:${PORT}/graphql`));
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
